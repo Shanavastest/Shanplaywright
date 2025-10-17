642554999b3e8d0c24ba38edb6a90e84eab76495
Shanavas Basheer <SBasheer@RCGGSCHN.IN>
2025-10-17 17:45:03 +0530

    secondcommit

--- tests/pracsubmit.spec.js
+++
@@ -1,5 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://live.origamirisk.com/Origami/Account/Login?ReturnUrl=%2fOrigami%2fProposals%2fInterview');
-});
